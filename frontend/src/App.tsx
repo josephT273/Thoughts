8335f8d3660127bf496ad60eec1cbd5e7ea42c01
Josepht273 <josephtadessem@gmail.com>
2024-10-21 10:33:14 +0300

    Post Area Input Box

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='homePage'>
+      <div className='navBar'>
+        <h1>Thoughts by <a href="https://x.com/josepht273">Joseph Tadesse</a></h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div className='postArea'>
+        <textarea rows={5} className='textArea' placeholder='Write note max allowed character is 200' maxLength={200}></textarea>
+        <input type="text" placeholder='Display Name' />
+        <div>
+          <button className='postButton'>Post</button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
